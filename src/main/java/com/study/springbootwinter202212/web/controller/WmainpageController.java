bc6d9a086a628a12ac8cadec664590d8515779f5
myeongseung8952 <myeongseung8952@gmail.com>
2022-12-28 19:01:38 +0900

    http://localhost:8000/mainpage

--- src/main/java/com/study/springbootwinter202212/web/controller/WmainpageController.java
+++ src/main/java/com/study/springbootwinter202212/web/controller/WmainpageController.java
@@ -1,10 +1,13 @@
 package com.study.springbootwinter202212.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/main")
 public class WmainpageController {
-
+    @GetMapping("/mainpage")
+    public String loadMainpage() {
+        return "/mainpage";
+    }
 }
